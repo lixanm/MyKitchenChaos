3f9eee0618dc37a8c39d6988eabc476f54d406e5
lixanm <143246847+lixanm@users.noreply.github.com>
2025-05-12 12:47:07 +0800

    移动厨房对象到另一个柜台上（改变厨房对象父级）

--- Assets/Scripts/Counter/ClearCounter.cs
+++ Assets/Scripts/Counter/ClearCounter.cs
@@ -2,17 +2,78 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class ClearCounter : MonoBehaviour
 {
-    [SerializeField]private KitchenObjectSO kitchenObjectSO;
-    [SerializeField]private Transform counterToPoint;
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
+    [SerializeField] private Transform counterToPoint;
 
+
+    //将物体转移到第二个柜台
+    [SerializeField] private ClearCounter sceondClearCounter;
+    [SerializeField] private bool testing;
+
+
+    //柜台要清楚是否有厨房对象放在上面
+    private KitchenObject kitchenObject;
+
+    private void Update()
+    {
+        //将物体转移到第二个柜台
+        if (testing &&Input.GetKeyUp(KeyCode.T))
+        {
+            if (kitchenObject != null)
+            {
+                kitchenObject.SetClearCounter(sceondClearCounter);
+            }
+        }
+
+    }
+
+    //互动
     public void Interact()
     {
-        //Debug.Log("交互");
-        Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterToPoint);
-        kitchenObjectTransform.localPosition = Vector3.zero;
+        if(kitchenObject == null)
+        {
+            Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterToPoint);
+            kitchenObjectTransform.GetComponent<KitchenObject>().SetClearCounter(this);
+        }
+        else
+        {
+            Debug.Log(kitchenObject.GetClearCounter());
+        }
 
-        Debug.Log(kitchenObjectTransform.GetComponent<KitchenObject>().GetKitchenObjectSO().objectName);
+        
+
     }
+
+
+    //台面点位置
+    public Transform GetKitchenObjectFollowTransform()
+    {
+        return counterToPoint;
+    }
+
+    //设置获取厨房对象
+    public void SetKitchenObject(KitchenObject kitchenObject)
+    {
+        this.kitchenObject = kitchenObject;
+    }
+    public KitchenObject GetKitchenObject()
+    {
+        return kitchenObject;
+    }
+
+    //清除柜台上的厨房对象
+    public void ClearKitchenObject()
+    {
+        kitchenObject = null;
+    }
+
+    //柜台上是否有厨房对象
+    public bool HasKitchenObject()
+    {
+        return kitchenObject != null;
+    }
+
 }
--- Assets/Scripts/KitchenObject.cs
+++ Assets/Scripts/KitchenObject.cs
@@ -8,8 +8,33 @@ public class KitchenObject : MonoBehaviour
 
     [SerializeField]private KitchenObjectSO kitchenObjectSO;
 
+    //确保厨房对象知道自己的位置
+    private ClearCounter clearCounter;
+
     public KitchenObjectSO GetKitchenObjectSO()
     {
         return kitchenObjectSO;
+    }
+
+    //改变父级
+    public void SetClearCounter(ClearCounter clearCounter)
+    {
+        this.clearCounter?.ClearKitchenObject();//清除原来的父级
+        
+        this.clearCounter = clearCounter;
+
+        if(clearCounter.HasKitchenObject())
+        {
+            Debug.LogError("柜台上已经有厨房对象了");
+        }
+
+        clearCounter.SetKitchenObject(this);//设置新的父级
+
+        transform.parent=clearCounter.GetKitchenObjectFollowTransform();//设置父级位置
+        transform.localPosition = Vector3.zero;
+    }
+    public ClearCounter GetClearCounter()
+    {
+        return clearCounter;
     }
 }
